96becf5ff5c2340d53e2f4e49bd0077c339c4e5e
Oğuzhan Çevik <oguzhancevik.developer@outlook.com>
2024-07-27 16:10:57 +0300

    inbox oubox worker service added

---
+++ src/Services/InOut.OrderPublisher.Service/Entities/OrderOutbox.cs
@@ -0,0 +1,10 @@
+﻿namespace InOut.OrderPublisher.Service.Entities;
+
+public class OrderOutbox
+{
+    public Guid IdempotentToken { get; set; }
+    public DateTime OccuredOn { get; set; }
+    public DateTime? ProcessedDate { get; set; }
+    public string Type { get; set; } = default!;
+    public string Payload { get; set; } = default!;
+}
---
+++ src/Services/InOut.OrderPublisher.Service/Jobs/OrderOutboxPublishJob.cs
@@ -0,0 +1,40 @@
+﻿using InOut.OrderPublisher.Service.Entities;
+using InOut.Shared.Events;
+using MassTransit;
+using Quartz;
+using System.Text.Json;
+
+namespace InOut.OrderPublisher.Service.Jobs;
+
+public class OrderOutboxPublishJob(IPublishEndpoint _publishEndpoint) : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+
+        if (OrderOutboxSingletonDatabase.DataReaderState)
+        {
+            OrderOutboxSingletonDatabase.DataReaderBusy();
+
+            List<OrderOutbox> orderOutboxes = (await OrderOutboxSingletonDatabase.QueryAsync<OrderOutbox>($@"SELECT * FROM ORDEROUTBOXES WHERE PROCESSEDDATE IS NULL ORDER BY OCCUREDON ASC")).ToList();
+
+
+            foreach (var orderOutbox in orderOutboxes)
+            {
+                if (orderOutbox.Type == nameof(OrderCreatedEvent))
+                {
+                    OrderCreatedEvent orderCreatedEvent = JsonSerializer.Deserialize<OrderCreatedEvent>(orderOutbox.Payload);
+
+                    if (orderCreatedEvent != null)
+                    {
+                        await _publishEndpoint.Publish(orderCreatedEvent);
+                        OrderOutboxSingletonDatabase.ExecuteAsync($"UPDATE ORDEROUTBOXES SET PROCESSEDDATE = GETDATE() WHERE IdempotentToken = '{orderOutbox.IdempotentToken}'");
+
+                    }
+                }
+            }
+
+            OrderOutboxSingletonDatabase.DataReaderReady();
+            await Console.Out.WriteLineAsync("Order outbox table checked!");
+        }
+    }
+}
